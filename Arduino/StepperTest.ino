b6b55d448900825b435ee526dec97557e8392087
Renjie "Jimmy" Li <jimmyleeyog@gmail.com>
2017-11-30 12:54:32 -0500

    Update StepperTest.ino

--- Arduino/StepperTest.ino
+++ Arduino/StepperTest.ino
@@ -17,9 +17,9 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Or, create it with a different I2C address (say for stacking)
 // Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
 
-// Connect a stepper motor with 200 steps per revolution (1.8 degree)
+// Connect a stepper motor with 48 steps per revolution (7.5 degree)
 // to motor port #2 (M3 and M4)
-Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(48, 2);
 
 
 void setup() {
@@ -29,23 +29,23 @@ void setup() {
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
-  myMotor->setSpeed(10);  // 10 rpm   
+  myMotor->setSpeed(60);  // 60 rpm   
 }
 
 void loop() {
   Serial.println("Single coil steps");
   myMotor->step(100, FORWARD, SINGLE); 
-  myMotor->step(100, BACKWARD, SINGLE); 
+  //myMotor->step(100, BACKWARD, SINGLE); 
 
-  Serial.println("Double coil steps");
-  myMotor->step(100, FORWARD, DOUBLE); 
-  myMotor->step(100, BACKWARD, DOUBLE);
+ // Serial.println("Double coil steps");
+  //myMotor->step(100, FORWARD, DOUBLE); 
+  //myMotor->step(100, BACKWARD, DOUBLE);
   
-  Serial.println("Interleave coil steps");
-  myMotor->step(100, FORWARD, INTERLEAVE); 
-  myMotor->step(100, BACKWARD, INTERLEAVE); 
+  //Serial.println("Interleave coil steps");
+  //myMotor->step(100, FORWARD, INTERLEAVE); 
+  //myMotor->step(100, BACKWARD, INTERLEAVE); 
   
-  Serial.println("Microstep steps");
-  myMotor->step(50, FORWARD, MICROSTEP); 
-  myMotor->step(50, BACKWARD, MICROSTEP);
+  //Serial.println("Microstep steps");
+  //myMotor->step(50, FORWARD, MICROSTEP); 
+  //myMotor->step(50, BACKWARD, MICROSTEP);
 }
